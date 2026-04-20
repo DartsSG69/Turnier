1338dad610412b734048532d8f6499c5ea9f1f6e
DartsSG69 <darts@sgsportfreunde69.de>
2026-04-20 10:56:26 +0200

    Update table.js

--- api/table.js
+++ api/table.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const url = `https://backend4.3k-darts.com/2k-backend4/api/v1/frontend/event/12059/phase/0/round/0/table?t=${Date.now()}`;
+    const url = `https://backend4.3k-darts.com/2k-backend4/api/v1/frontend/event/12059/phase/0/round/0/tablet=${Date.now()}`;
     // const url = `https://backend5.3k-darts.com/2k-backend5/api/v1/frontend/event/19628?t=${Date.now()}`;
     // Score SG Turnier - const url = `https://backend6.3k-darts.com/2k-backend6/api/v1/frontend/event/983/phase/5529?t=${Date.now()}`;
 
