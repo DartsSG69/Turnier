3c62894de8e72ca84c487eddd48d0a6d1a5f413f
DartsSG69 <darts@sgsportfreunde69.de>
2026-04-20 11:48:10 +0200

    Update performance.js

--- api/performance.js
+++ api/performance.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
-    const url = `https://backend6.3k-darts.com/2k-backend6/api/v1/frontend/event/3973/performance?t=${Date.now()}`;
-//Score SG Turnier - const url = `https://backend6.3k-darts.com/2k-backend6/api/v1/frontend/event/983/performance?t=${Date.now()}`;
+ //   const url = `https://backend6.3k-darts.com/2k-backend6/api/v1/frontend/event/3973/performance?t=${Date.now()}`;
+const url = `https://backend6.3k-darts.com/2k-backend6/api/v1/frontend/event/983/performance?t=${Date.now()}`; //Turnier SG
 
     const response = await fetch(url, {
       headers: {
